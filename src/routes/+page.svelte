f64ec13a0b6644ed505cadf2c24d65dffdec9683
Caro Appleby <caroline.appleby@cantab.net>
2023-10-20 19:59:18 +0100

    Very basic filtering of sets on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,19 @@
 
 	export let data;
 	$: folder = data.folder;
-	$: visibleSections = folder.content;
+	$: visibleSections = folder.content.filter((f) => f.visible == true);
+	function resetFilters() {
+		folder.content.forEach((f) => (f.visible = true));
+		folder.content = folder.content;
+	}
 </script>
 
 <h1>Welcome to Choonbook</h1>
 <ul>
 	{#each folder.content as section}
-		<li>{section.name}</li>
+		<li><label><input bind:checked={section.visible} type="checkbox" />{section.name}</label></li>
 	{/each}
+	<li><button on:click={resetFilters}>Any type of tune</button></li>
 </ul>
 
 <div class="set-list">
@@ -35,5 +40,18 @@
 	.set-list {
 		@apply flex flex-wrap gap-6 justify-evenly;
 		@apply max-w-6xl mx-auto;
+	}
+
+	button {
+		@apply rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600;
+	}
+	ul {
+		@apply p-2;
+	}
+	input[type='checkbox'] {
+		@apply mr-1;
+	}
+	li {
+		@apply my-2;
 	}
 </style>
