a92abff40477f8f5037c897a9fa50991d6bb6ba8
Caro Appleby <caroline.appleby@cantab.net>
2023-10-20 20:35:05 +0100

    Load homepage with all tune types visible

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,5 +4,7 @@ import type { Folder } from './folder.json/+server';
 export function load({ fetch }: LoadEvent): Promise<{ folder: Folder }> {
 	return fetch('/folder.json')
 		.then((res) => res.json())
-		.then((folder) => ({ folder }));
+		.then((folder) => ({
+			folder: { ...folder, content: folder.content.map((c) => ({ ...c, visible: true })) }
+		}));
 }
