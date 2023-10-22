9391036ecdc86ea6b07de79c19bbe441ffe5cdb7
James Rhodes <jarhodes314@gmail.com>
2023-10-22 16:05:25 +0100

    Deploy demo version of site

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import type { LoadEvent } from '@sveltejs/kit';
 import type { Folder } from '$lib/types/index.js';
 
+export const prerender = true;
+
 export function load({ fetch }: LoadEvent): Promise<{ folder: Folder }> {
 	return fetch('/folder.json')
 		.then((res) => res.json())
