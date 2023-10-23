f9ddc651b9bd49040be5832dd689b580a3bca28b
Caro Appleby <caroline.appleby@cantab.net>
2023-10-23 18:11:06 +0100

    Fix formatting oop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,17 +9,23 @@
 </svelte:head>
 
 <h1>{data.folder.name}</h1>
-<p >
+<p>
 	Here's an overview of all the sets and tunes in your folder. You can get a quick reminder of how
 	each tune starts on this page, and see notes about the sets if you've added them. You can filter
 	by type of tune, or however you like to break up your folder into sections. Clicking the title of
 	a set will show you all the dots for that set.
 </p>
+
 <div class="filterable-sets-grid">
 	<FilterableSetsGrid folder={data.folder} tuneFont="sans-serif" />
 </div>
 
-<p>When a user interacts with a <code>ViewSet</code> component, some preferences are kept in localstorage. This means user preferences are remembered per browser without having to use any kind of database or syncing service. By default, we store the transposition of each tune in a set, and the zoom level for the set at each device orientation.</p>
+<p>
+	When a user interacts with a <code>ViewSet</code> component, some preferences are kept in localstorage.
+	This means user preferences are remembered per browser without having to use any kind of database or
+	syncing service. By default, we store the transposition of each tune in a set, and the zoom level for
+	the set at each device orientation.
+</p>
 
 <style lang="postcss">
 	.filterable-sets-grid {
