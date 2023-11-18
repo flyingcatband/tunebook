1a1530322de3a8db69367570d3f65d382188cec9
Caro Appleby <caroline.appleby@cantab.net>
2023-11-18 11:14:48 +0000

    Expose keyedLocalStorage to enable global transpose

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,3 +3,4 @@ export { default as Incipit } from './Incipit.svelte';
 export { default as FilterableSetsGrid } from './FilterableSetsGrid.svelte';
 export { default as FilterSets } from './FilterSets.svelte';
 export { default as SetPreview } from './SetPreview.svelte';
+export { keyedLocalStorage } from './keyedLocalStorage.js';
