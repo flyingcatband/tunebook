96d791bc33be4811b2ffb112dd0067d4da1058f3
James Rhodes <jarhodes314@gmail.com>
2023-12-18 21:55:11 +0000

    Add more customisation options and docs to `SetPreview`

--- src/lib/SetPreview.svelte
+++ src/lib/SetPreview.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
 	import Incipit from '$lib/Incipit.svelte';
 	import type { Set } from '$lib/types/index.js';
-	/** the path to where your sets pages will be */
+	/** The path to where your sets pages will be */
 	export let basePath = '/';
+	/** The set to show in the preview */
 	export let set: Set;
 	/** Should the set-level text notes be displayed? */
 	export let showNotes = true;
+	/** The font family to use for text rendered as part of the ABC */
 	export let tuneFont: string | undefined = undefined;
+	/** A list of ABC headers to display, specified as a string (e.g. 'TCBN') */
 	export let displayAbcFields: string | undefined = undefined;
+	/** The element to use for the heading. Set to undefined to hide the heading. */
+	export let headingTy: string | undefined = 'h2';
 </script>
 
 <a href={basePath + set.slug}>
 	<div class="set-preview">
-		{#if set.name != undefined}
-			<h2>{set.name}</h2>
+		{#if set.name != undefined && headingTy}
+			<svelte:element this={headingTy}>{set.name}</svelte:element>
 		{/if}
 		{#if set.tags.length}
 			<ul class="tag-list">
