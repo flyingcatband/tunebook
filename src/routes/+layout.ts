46de8d6d655012f003b538a50d25aca44742bd66
James Rhodes <jarhodes314@gmail.com>
2025-03-09 10:00:01 +0000

    Acknowledge base path exists in the links we generate

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,11 @@
 import type { LoadEvent } from '@sveltejs/kit';
 import type { Folder } from '$lib/types/index.js';
+import { base } from '$app/paths';
 
 export const prerender = true;
 
 export function load({ fetch }: LoadEvent): Promise<{ folder: Folder }> {
-	return fetch('/folder.json')
+	return fetch(`${base}/folder.json`)
 		.then((res) => res.json())
 		.then((folder) => ({
 			folder
