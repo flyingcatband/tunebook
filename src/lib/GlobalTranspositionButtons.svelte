f2e563938fbbd4122f123d676710074313a8cdae
James Rhodes <jarhodes314@gmail.com>
2025-03-09 11:50:52 +0000

    Make global transposition buttons component

---
+++ src/lib/GlobalTranspositionButtons.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { keyedLocalStorage } from './keyedLocalStorage';
+
+	export let globalTransposition = keyedLocalStorage('globalTransposition', 0);
+</script>
+
+<p class="mx-auto w-fit flex flex-wrap gap-2">
+	<button onclick={() => ($globalTransposition = 2)}>Make the folder B♭</button>
+	<button onclick={() => ($globalTransposition = 0)}>Make the folder C</button>
+	<button onclick={() => ($globalTransposition = -3)}>Make the folder E♭</button>
+</p>
